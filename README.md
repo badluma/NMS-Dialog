796721f25a1f3f322c37b3d50144bccb4f67739c
badluma <commune-nice.5w@icloud.com>
2025-11-07 14:33:54 -0600

    Enhanced commenting
    - Added many comments to the code
    - Simplified some code

--- README.md
+++ README.md
@@ -1,4 +1,24 @@
 # NMS Dialog
 *(I'll find a better name for this)*
 
+---
+
+### Story and Gameplay
 This game is a simple, text based game for the 3DS that is inspired by the game No Man's Sky.
+It follows the story of a traveller that woke up in the middle of nowhere and is trying to get into space. The player can interact with the story by making decisions, pressing buttons to interact with the environment and more.
+
+### Installation
+To install this game on your 3DS, download the ```NMS-Dialog.3dsx``` file and drag it into the ```/3ds``` folder on your SD-Card. After you have done that, turn on your 3DS and launch the Homebrew Launcher. Once the app opened, scroll down with the D-Pad until you see an app called NMS-Dialog. Open it with A and the app will launch.
+
+### Important Sidenotes
+**Future of the game**
+I am new to C++ developement, and I have other stuff to do that is more important than programming a 3DS game. I can't promise to completely finish this game, as I started building it to learn C++. So, please don't be too disappointed in case I won't finish it.
+
+**Information about the use of AI**
+This game is coded by me alone. Local AI *(qwen2.5-coder:7b)* was sometimes used for troubleshooting and some coding questions.
+
+---
+
+Have fun!
+
+Made with ♥︎ by [badluma](github.com/badluma)
--- source/main.cpp
+++ source/main.cpp
@@ -20,8 +20,8 @@ const std::string CYAN = "\x1b[36m";
 const std::string ACC_COLOR = RED;
 
 // Variables
-bool waitForPress = false;
-int choice;
+bool waitForPress = false; // Initializes the waitForPress variable and sets it to false 
+int choice; // Initializes the choice variable
 PrintConsole topScreen, bottomScreen; // Screens to print on
 
 // Function identifiers
@@ -36,6 +36,10 @@ void holdButton(u32 button, int duration);
 void quitOnPress();
 void showChoice(std::string option1, std::string option2);
 void describe(std::string text, double time, bool doEndl);
+
+
+// ----------------------------------------------------------------------------------------------------
+
 
 // Main Function
 int main(int argc, char* argv[])
@@ -59,13 +63,10 @@ int main(int argc, char* argv[])
     para(12);
     show("        Hold (Y) to initialize", 1, false, RESET);
     holdButton(KEY_Y, 2000);
-    consoleClear();
-    switchScreen(1);
-    consoleClear();
+    clear(0);
     wait(2000); // Wait for 2 seconds
 
     // ------------- AWAKENING ------------
-    para(1);
     show("ATLAS SYSTEM INITIALIZATION ... ", 1, false, RESET);
     show("       COMPLETED", 1.5, true, ACC_COLOR);
     wait(1000); // Wait for 1 seconds
@@ -121,11 +122,34 @@ int main(int argc, char* argv[])
         show("The scanner isn't too important right now.", 1, true, RESET);
         show("For now, I want to find out where I am.", 1, true, RESET);
         describe("I'm slowly heading towards the forest", 1, true);
+        para(1);
+        show("SCANNER STATUS ...               ", 1, false, RESET);
+        show("NON-OPERATIONAL", 1, true, ACC_COLOR);
+        show("REQUIRED MATERIAL: ", 1, false, RESET);
+        show("SODIUM", 1, true, ACC_COLOR);
+        para(1);
     }
+
+    switchScreen(1);
+    show("Perfect, there is sodium here.", 1, true, RESET);
+    show("It looks like it's in a small rock formation. ", 1, true, RESET);
+    switchScreen(2);
+    holdButton(KEY_Y, 7000);
+
     wait(10000);
     gfxExit();
     return 0;
 }
+
+
+
+
+
+// ----------------------------------------------------------------------------------------------------
+
+
+
+
 
 // Function defenitions
 void switchScreen(int screen) {
@@ -137,42 +161,42 @@ void switchScreen(int screen) {
     }
 }
 void para(int count) {
-    for (int i = 0; i < count; i++) 
-        std::cout << std::endl;
+    for (int i = 0; i < count; i++) // Repeats the loop the number of times that is put in the count parameter
+        std::cout << std::endl; // Start the new line
     std::cout << " "; // Type a space to have a border at the edge of the screen
 }
 void wait(int timeToWait) {
     std::this_thread::sleep_for(std::chrono::milliseconds(timeToWait));
 }
 void show(std::string text, double time, bool doEndl, std::string color) {
-    std::cout << color;
-    for (char character : text) {
-        std::cout << character;
-        if (!(character == ' ')) {
-            wait(100 * time);
+    std::cout << color; // Sets console color to the parameter "color"
+    for (char character : text) { // Repeats for every character in the string
+        std::cout << character; // Prints the character
+        if (!(character == ' ')) { // Doesn't wait if the character is a space
+            wait(100 * time); // Waits a short time before the next character is displayed to create a typing effect
         }
     }
-    if (doEndl) {
-        std::cout << std::endl << " ";
+    if (doEndl) { // Only runs if doEndl is true
+        std::cout << std::endl << " "; // Sets a space before each new line to create a border at the edge
     }
-    wait(250);
+    wait(250); // Wait a short time before showing the next message
 }
 void color(std::string colorName) {
-    std::cout << colorName;
+    std::cout << colorName; // Sets console color to the parameter "color"
 }
 void clear(int screen) {
     if (screen == 1) { // Clear top screen
         switchScreen(1);
         consoleClear();
         std::cout << std::endl; // Add border on the top of the screen through an empty line
-        std::cout << " "; // Type space at end to have a border
+        std::cout << " "; // Type space at end to have a border at the edge of the screen
         
     }
     if (screen == 2) { // Clear bottom screen
         switchScreen(2);
         consoleClear();
         std::cout << std::endl; // Add border on the top of the screen through an empty line
-        std::cout << " "; // Type space at end to have a border
+        std::cout << " "; // Type space at end to have a border at the edge of the screen
     }
     if (screen == 0) { // Clear both screens
         switchScreen(2);
@@ -180,87 +204,87 @@ void clear(int screen) {
         switchScreen(1);
         consoleClear();
         std::cout << std::endl; // Add border on the top of the screen through an empty line
-        std::cout << " "; // Type space at end to have a border
+        std::cout << " "; // Type space at end to have a border at the edge of the screen
     }
 }
 void button(u32 button) {
     waitForPress = true;
     while (waitForPress && aptMainLoop()) { // While the function is waiting for a button press, 
         gspWaitForVBlank();
-        hidScanInput();                     // it scans for input with hidScanInput();
+        hidScanInput(); // it scans for input with hidScanInput();
         
-        u32 kDown = keysDown(); 
+        u32 kDown = keysDown(); // Sets kDown variable to button that is pressed
 
         if (kDown & button) {            
-            waitForPress = false;           // When the button is pressed, it stops listening for input and breaks the function
+            waitForPress = false; // When the button is pressed, it stops listening for input and breaks the function
         }
     }
 }
 void holdButton(u32 button, int duration) {
-    waitForPress = true;
-    while (waitForPress && aptMainLoop()) {
+    waitForPress = true; // Sets waitForPress to true to start the loop
+    while (waitForPress && aptMainLoop()) { // Runs until a button is pressed
         gspWaitForVBlank();
-        hidScanInput();
+        hidScanInput(); // Scans for input
         
-        u32 kDown = keysDown(); 
+        u32 kDown = keysDown(); // Sets kDown variable to button that is pressed
         
-        if (kDown & button) {
+        if (kDown & button) { // Only runs if a button is pressed
             wait(2000);
-            if (kDown & button) {
-                waitForPress = false;
-                consoleClear();
-                switchScreen(1);
-                consoleClear();
+            if (kDown & button) { // Checks after the time is up if the button is still pressed
+                waitForPress = false; // Sets waitForPress to false to break the loop
+                consoleClear();  // Clear
+                switchScreen(1); // both
+                consoleClear();  // consoles
             }
             else {
-                holdButton(button, duration);
+                holdButton(button, duration); // If button is released, the function is started from the beginning
             }
         }
     }
 }
-void quitOnPress() {
-    while (aptMainLoop()) {
-        button(KEY_START);
-        break;
+void quitOnPress() { // Function made to quit the program when the START key is pressed; not yet working
+    while (aptMainLoop()) { // Runs runs always when the program is active
+        button(KEY_START); // Checks if the START button is pressed using the button() function
+        break; // Quits the program
     }
 }
 void showChoice(std::string option1, std::string option2) {
     switchScreen(2); // Switch to bottom screen
     para(14);
-    show("(A) ", 1, false, ACC_COLOR);
-    show(option1, 1, true, RESET);
-    para(1);
-    if (!(option2.empty())) {
-        show("(B) ", 1, false, ACC_COLOR);
-        show(option2, 1, true, RESET);
+    show("(A) ", 1, false, ACC_COLOR); // Shows the key to press
+    show(option1, 1, true, RESET); // Shows the first option from the parameter
+    para(1); // Type a paragraph
+    if (!(option2.empty())) { // If second option exists
+        show("(B) ", 1, false, ACC_COLOR); // Shows key to press
+        show(option2, 1, true, RESET); // Shows the second option from the parameter
     }
-    waitForPress = true;
-    while (waitForPress && aptMainLoop()) {
-        gspWaitForVBlank();
-        hidScanInput();
+    waitForPress = true; // Sets waitForPress variable to true to run the loop below
+    while (waitForPress && aptMainLoop()) { // Repeats the loop until a button is pressed
+        gspWaitForVBlank(); 
+        hidScanInput(); // Scans for input
         
-        u32 kDown = keysDown(); 
+        u32 kDown = keysDown(); // Sets the variable kDown to the key that is pressed at the moment
         
-        if (kDown & KEY_A) {
-            waitForPress = false;
-            choice = 1;
-            consoleClear();
-            std::cout << std::endl;
-            break;
+        if (kDown & KEY_A) { // Checks if the key A is pressed
+            waitForPress = false; // Sets waitForPress to false to break the loop
+            choice = 1; // Sets the global choice variable to 1
+            consoleClear(); // Clears the bottom screen console
+            std::cout << std::endl << " "; // Type space at end to have a border at the edge of the screen
+            break; // Break the loop
         }
         
-        if (kDown & KEY_B && !(option2.empty())) {
-            waitForPress = false;
-            choice = 2;
-            consoleClear();
-            std::cout << std::endl;
-            break;
+        if (kDown & KEY_B && !(option2.empty()) ) { // Checks if the key B is pressed and if a second option exists
+            waitForPress = false; // Sets waitForPress to false to break the loop
+            choice = 2; // Sets the global choice variable to 2
+            consoleClear(); // Clears the bottom screen console
+            std::cout << std::endl << " "; // Type space at end to have a border at the edge of the screen
+            break; // Break the loop
         }
     }
 }
 void describe(std::string text, double time, bool doEndl) {
-    text = "*" + text + "*";
-    para(1);
-    show(text, time, doEndl, GRAY);
+    text = "*" + text + "*"; // Wraps the text in two *-characters
+    para(1); 
+    show(text, time, doEndl, GRAY); // Shows the text in gray
     para(1);
 }
